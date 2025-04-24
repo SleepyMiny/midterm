a06c2f939a2de3f1ca4e251ab420278508848bc6
minyoung <dark47297@naver.com>
2025-04-24 13:34:11 +0900

    3

--- src/App.js
+++ src/App.js
@@ -2,20 +2,22 @@ import React, { useState } from "react";
 import "./App.css";
 
 function Midterm() {
-  const [selectedFruit, setSelectedFruit] = useState("");
+  const [selectedFruit, setSelectedFruit] = useState("");      // 선택된 과일 상태를 저장하는 state 훅
 
+
+  // 과일 버튼 클릭 시 호출되는 함수
   const handleFruitClick = (fruit) => {
-    setSelectedFruit(fruit);
+    setSelectedFruit(fruit);          // 클릭된 과일 이름을 state에 저장함
   };
 
-  return (
+  return ( 
     <div>
-      <div style={{ display: "flex", alignItems: "center"}}>
+      <div style={{ display: "flex", alignItems: "center"}}>  {/* 과일이 질문 오른쪽으로 오는 스타일 */}
         <p>당신은 어떤 과일을 좋아하시나요?</p>
         <strong>{selectedFruit && ` ${selectedFruit}`}</strong>
       </div>
 
-      <div style={{ marginTop: "10px" }}>
+      <div>
         <button onClick={() => handleFruitClick("사과")}>사과</button>
         <button onClick={() => handleFruitClick("오렌지")}>오렌지</button>
         <button onClick={() => handleFruitClick("바나나")}>바나나</button>
@@ -24,6 +26,7 @@ function Midterm() {
   );
 }
 
+// 컴포넌트가 화면에 렌더링되는 부분
 function App() {
   return (
     <div>
